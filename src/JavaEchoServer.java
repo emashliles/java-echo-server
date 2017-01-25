d69af6af5d3d4f7f82a7a01204b1161af5c190cb
EA <emashliles@gmail.com>
2017-01-25 17:28:21 +0000

    Test that input can be echoed.

--- src/JavaEchoServer.java
+++ src/JavaEchoServer.java
@@ -1,8 +1,15 @@
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.util.Scanner;
 
 public class JavaEchoServer {
 
     public static void main(String args[]){
-        System.out.print("Hello");
+        System.out.print("Please enter text to be echoed:");
+        echoInput();
+    }
+
+    private static void echoInput() {
+        Scanner sc = new Scanner(System.in);
+        String echoText = sc.nextLine();
+        System.out.print(echoText);
     }
 }
--- test/JavaEchoServerTest.java
+++ test/JavaEchoServerTest.java
@@ -2,6 +2,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
@@ -10,22 +11,40 @@ import static org.junit.Assert.assertEquals;
 public class JavaEchoServerTest {
 
     private ByteArrayOutputStream stdOut;
+    private ByteArrayInputStream stdIn;
 
     @Before
     public void setUp() throws Exception {
         stdOut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(stdOut));
+
     }
 
     @After
     public void tearDown() throws Exception {
         System.setOut(null);
+        System.setIn(null);
     }
 
     @Test
-    public void serverCanWriteInput() {
+    public void asksForInput() {
         String[] args = {};
+        stdIn = new ByteArrayInputStream("\n".getBytes());
+        System.setIn(stdIn);
+
         JavaEchoServer.main(args);
-        assertEquals("Hello", stdOut.toString());
+        assertEquals("Please enter text to be echoed:", stdOut.toString());
+    }
+
+    @Test
+    public void echoInputOnce() {
+        String[] args = {};
+        stdIn = new ByteArrayInputStream("Echo\n".getBytes());
+        System.setIn(stdIn);
+
+        JavaEchoServer.main(args);
+        assertEquals("Please enter text to be echoed:Echo", stdOut.toString());
+
+
     }
 }
